9dd68e34862a99ba4843c9492c4951471096df4e
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2023-03-19 11:22:53 -0500

    Got the boundary class's sample method working

--- axis_oriented_polygon_alpha.py
+++ axis_oriented_polygon_alpha.py
@@ -20,7 +20,6 @@ def get_corners(intervals):
 def get_intervals(corners):
     # TODO this func would benefit from a check that cell has the right shape
     """
-
     Undo what get_corners does to a list of intervals,
     e.g., recover the intervals [ [a,b], [c,d], [e,f] ]
     from the corners [ [b,d,f], [a,c,e] ].
@@ -63,7 +62,7 @@ def measure_multiple_cells( list_of_corner_pairs, volume=True ):
         )
 
 
-class ntangle_beta:
+class axis_oriented_polygon:
     '''
     fields:
         cells,
@@ -102,8 +101,10 @@ class ntangle_beta:
         # ~~~ throw away and degenerate cells
         non_degenerate_ones = np.where( np.all(lengths_of_each_side>0, axis=0) )
         if len(non_degenerate_ones[0])<len(list_of_cells):
-            list_of_cells = list_of_cells[non_degenerate_ones]
-            warnings.warn("Degenerate cells discarded.")
+            warnings.warn("Degenerate cells detected.")
+            if enforce_disjoint_interiors:
+                list_of_cells = list_of_cells[non_degenerate_ones]
+                warnings.warn("Degenerate cells discarded.")
         #
         # ~~~ assign attributes
         self.cells  =  list_of_cells
@@ -144,7 +145,7 @@ class ntangle_beta:
                 A = INTERVALS[:, 0]
                 B = INTERVALS[:, 1]
                 if np.all( (B > a) & (b > A) ):
-                    raise ValueError("Cells have non-disjoint interiors, i.e., they overlap. This will cause issues when sampling.")
+                    warnings.warn("Cells have non-disjoint interiors, i.e., they overlap. This will cause issues when sampling.")
     #
     #   sample a point at random from a randomly chosen cell
     #
@@ -160,9 +161,13 @@ class ntangle_beta:
         low_corners  =  np.array([cell[1] for cell in self.cells])
         sampled_points  =  np.random.uniform( size=(N,self.dimension) )
         sampled_points  =  sampled_points * lengths[which_cell] + low_corners[which_cell]
-        if with_time==None:
+        #
+        #~~~ add a time component if so desired (same as in the parent class)
+        if with_time is None:
             pass # do nothing
         else:
+            if isinstance( with_time, int ):
+                with_time  =  [with_time, with_time]
             if len(with_time)==2:
                 initial_time  =  with_time[0]
                 final_time    =  with_time[1]
@@ -174,11 +179,9 @@ class ntangle_beta:
 def open_cells_intersect( cell, CELL ):
     # todo rewrite to accept cells of either possible format, and call get_intervals instead of repeating its guts
     """
-
     first, convert the list of corners into a list of intervals,
     e.g., convert corners=[ [b,d,f], [a,c,e] ] into intervals [ [a,b], [c,d], [e,f] ]
     note that the transopose alone gives [ [b,a], [d,c], [f,e] ]
-
     """
     cell = np.array(cell).T
     CELL = np.array(CELL).T
@@ -196,7 +199,6 @@ def open_cells_intersect( cell, CELL ):
         A = np.array(CELL[1])
         B = np.array(CELL[0])
     """
-
     Claim. Assuming a<A and b<B, we have that...
        ... intervals (a,b) and (A,B) intersect IFF both B>a and b>A.
     
@@ -210,23 +212,17 @@ def open_cells_intersect( cell, CELL ):
         As well, A<B by assumption, so the latter holds IFF a<B.
         By substitution, (a,b)\cap(A,B) is non-empty IFF both A<b and a<B.
             QED.
-
     This claim tells us how to compute whether open intervals I_j and H_j intersect.
     Think of cell=\prod_jI_j and CELL=\prod_jH_j.
     Using the claim, we now compute the indices j for which I_j \cap H_j is non-empty.
-
     """
     indices_with_intersection = (B > a) & (b > A)
     """
-
     Finally, recall the distributive law
-
         (\prod_jI_j) \cap (\prod_jH_j) = \prod_j (I_j \cap H_j)
-
     and that a Cartesian product is non-empty IFF so is each "Cartesian multiplicand."
     Therfore, cell \cap CELL is non-empty IFF I_j \cap H_j is non-empty for every j,
     that is to say IFF every index j is in indices_with_intersection.
-
     """
     non_empty = np.all(indices_with_intersection)   # True IFF indices_with_intersection is all True
     if non_empty:
@@ -235,6 +231,76 @@ def open_cells_intersect( cell, CELL ):
     else:
         intersection = None
     return non_empty, intersection
+
+
+
+
+class boundary(axis_oriented_polygon):
+    def __init__( self, list_of_lists_of_intervals, list_of_integers, list_of_floats ):
+        super().__init__( list_of_lists_of_intervals=list_of_lists_of_intervals, enforce_disjoint_interiors=False )
+        self.missing_dimension  =  np.array(list_of_integers)
+        self.missing_value  =  np.array(list_of_floats)
+    def sample( self, N, with_time=None ):
+        #
+        # ~~~ decide which cell to sample (same as in the parent class)
+        d  =  self.dimension
+        number_of_cells  =  len(self.cells)
+        which_cell  =  np.random.choice(np.arange(number_of_cells), N, p=self.weights)
+        #
+        # ~~~ sample the appropriate number of points from each cell (same as in the parent class)
+        lengths  =  self.lengths_of_each_side
+        low_corners  =  np.array([cell[1] for cell in self.cells])
+        sampled_points  =  np.random.uniform( size=(N,d) )
+        sampled_points  =  sampled_points * lengths[which_cell] + low_corners[which_cell]
+        #
+        #~~~ embed into one higher spatial dimension
+        """
+        Transform every row of the array sampled_points such that:
+            - the i-th row of sampled_points is replaced by a vector of length d+1
+            - where the new entry in this row is derived by taking the integer value k=which_cell[i]
+            - and the new vector of length d+1 has value missing_value[k] in entry missing_dimension[k].
+        For example:
+
+# given the data...
+d = 4
+x_row = np.zeros(d)
+k = 2
+val = 1
+x_new = np.insert(x_row, k, val)
+x_new
+
+# ...the command...
+np.insert( A.flatten(), np.arange(10)*4 + missing_dimension, missing_value ).reshape((10,5))
+
+# ...creates the same matrix as the one called "A_new" in the block:
+A = np.ones((10,4))
+A_new = np.zeros((10,5))
+missing_dimension = np.arange(10)%5
+missing_value = np.arange(10)+10
+for row in range(A.shape[0]):
+    k = missing_dimension[row]
+    val = missing_value[row]
+    A_new[row,:] = np.insert(A[row,:], k, val)
+
+        """
+        sampled_points  =  np.insert(
+                sampled_points.flatten(),
+                np.arange(N)*d + self.missing_dimension[which_cell],
+                self.missing_value[which_cell]
+            ).reshape((N,d+1))
+        #
+        #~~~ add a time component if so desired (same as in the parent class)
+        if with_time is None:
+            pass # do nothing
+        else:
+            if isinstance( with_time, int ):
+                with_time  =  [with_time, with_time]
+            if len(with_time)==2:
+                initial_time  =  with_time[0]
+                final_time    =  with_time[1]
+                times = np.random.uniform( initial_time, final_time, size=(N,1) )
+                sampled_points = np.hstack(( sampled_points, times ))
+        return sampled_points
 
 
 
@@ -265,10 +331,10 @@ open_cells_intersect( get_corners(bad[0]), get_corners(bad[1]) )
 open_cells_intersect( get_corners(bigger[0]), get_corners(bigger[1]) )
 
 # (ex.4) runs and doen't return anything
-Omega = ntangle_beta(good)
+Omega = axis_oriented_polygon(good)
 
-# (ex.5) provokes ValueError: Cells have non-disjoint interiors...
-Prompts_Error = ntangle_beta(bad)
+# (ex.5) provokes the warning: Cells have non-disjoint interiors...
+Prompts_Error = axis_oriented_polygon(bad)
 
 # (ex.6) equivalent to ex.1, returns (False, None)
 open_cells_intersect( Omega.cells[0], Omega.cells[1] )
@@ -276,5 +342,16 @@ open_cells_intersect( Omega.cells[0], Omega.cells[1] )
 # (ex.7) sample 10 points chosen uniformlt at random from Omega = ([0,5]x[0,2]) \setminus ([0,1)X[0,1))
 Omega.sample(10)
 
+# (ex.8) boundary of the cell [-9,0]x[1,2]
+bd = boundary( [
+            [[1,2]],
+            [[1,2]],
+            [[-9,0]],
+            [[-9,0]]
+        ],
+        [ 0,   0,   1,   1],
+        [ 0., -9.,  1.,  2.]
+    )
+bd.sample( 20, with_time=[10,11] )
 
 #
